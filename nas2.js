72c10b2a8cd71d08ad23f5848caa731f334a83be
harryge00 <harryge00@gmail.com>
2018-05-27 23:40:34 +0800

    almost completed

--- nas2.js
+++ nas2.js
@@ -1,97 +1,69 @@
 'use strict';
 
 var SlotMachine = function () {
-  LocalContractStorage.defineProperty(this, "balance", {
-    stringify: function (obj) {
-        return obj.toString();
-    },
-    parse: function (str) {
-        return new BigNumber(str);
-    }
-  });
-  LocalContractStorage.defineProperty(this, "prizes", null);
+  // LocalContractStorage.defineProperty(this, "balance", {
+  //   stringify: function (obj) {
+  //       return obj.toString();
+  //   },
+  //   parse: function (str) {
+  //       return new BigNumber(str);
+  //   }
+  // });
 };
 
 // save value to contract, only after height of block, users can takeout
 SlotMachine.prototype = {
   init: function (balance) {
     Math.random.seed("I am a fucking genuis. ~~");
-    this.prizes = 
+    LocalContractStorage.set("balance", 0);
   },
 
   spin: function () {
+    var topPrize = new BigNumber(0.007);
+    var secPrize = new BigNumber(0.003);
+    var thrPrize = new BigNumber(0.002);
+    var balance = new BigNumber(LocalContractStorage.get("balance"));
+    var prizes = [secPrize, thrPrize, thrPrize, thrPrize, secPrize, topPrize];
     var from = Blockchain.transaction.from;
     var value = Blockchain.transaction.value;
-    this.pool.balance.plus(value);
+    balance.plus(value);
     if(value.lt(0.001)) {
+      LocalContractStorage.set("balance", balance.toNumber());
       throw new Error("Insufficient nas to play!");
     }
     var arr = [];
     for(var i = 0; i < 3; i++) {
-      arr[i] = Math.floor(Math.random() * this.prizes.length)
+      arr[i] = Math.floor(Math.random() * prizes.length)
     }
     if(arr[0] == arr[1] && arr[1] == arr[2]) {
-      var prize = this.pool.prizes[arr[0]];
-      if(prize.gt(this.pool.balance)) {
+      var prize = prizes[arr[0]];
+      if(prize.gt(balance)) {
+        LocalContractStorage.set("balance", balance.toNumber());
         throw new Error("Oops...Prize pool is insufficient!");
       }
       var res = Blockchain.transfer(address, prize);
       if(!res) {
         console.log("Transaction failed!");
       }
-      this.pool.balance.minus(prize);
-      // switch(arr[0]) {
-      //   case 5:
-      //     // Lucky 7
-      //     var prize = new BigNumber(0.007);
-      //     var res = Blockchain.transfer(address, prize);
-      //     if(!res) {
-      //       console.log("transfer failed!");
-      //     }
-      //     break;
-      //   case 4:
-      //     // bar
-      //     var prize = new BigNumber(0.003);
-      //     var res = Blockchain.transfer(address, prize);
-      //     if(!res) {
-      //       console.log("transfer failed!");
-      //     }
-      //     break;
-      //   case 0:
-      //     // cherry
-      //     var prize = new BigNumber(0.003);
-      //     var res = Blockchain.transfer(address, prize);
-      //     if(!res) {
-      //       console.log("transfer failed!");
-      //     }
-      //     break;
-      //   default:
-      //     var prize = new BigNumber(0.001);
-      //     var res = Blockchain.transfer(address, prize);
-      //     if(!res) {
-      //       console.log("transfer failed!");
-      //     }
-      // }
+      balance.minus(prize);
+      LocalContractStorage.set("balance", balance.toNumber());
+      
     }
     return arr;
   },
 
   balanceOf: function () {
-    // return this.pool.balance;
-    return LocalContractStorage.get("testBalance");
+    return LocalContractStorage.get("balance");
   },
   add: function(i) {
-    var bal = new BigNumber(LocalContractStorage.get("testBalance"));
-    bal.plus(i);
-    LocalContractStorage.set("testBalance", bal);
-    return bal;
-  },
-  getPrize: function(i) {
-    if(i < 0 || i >= this.pool.prizes.length) {
-      throw new Error("Illegal argument!");
+    if(i > 0.000000001) {
+      var balance = new BigNumber(LocalContractStorage.get("balance"));
+      balance.plus(i);
+      LocalContractStorage.set("balance", balance.toNumber());
     }
-    return this.pool.prizes[i];
+    return balance.toNumber();
   },
+  
 
   verifyAddress: function (address) {
     // 1-valid, 0-invalid
